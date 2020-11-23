6d4d214b5a80584dd150ad1cff5750b7148c4013
Florian SIMUNEK <florian.simunek@gmail.com>
2020-11-23 18:09:29 +0100

    Finished script, added hex-value text

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
 const changeColorBtn = document.querySelector('button');
 const body = document.querySelector('body');
+const hexValueText = document.getElementById('hex-value');
 
 changeColorBtn.addEventListener('click', changeBackgroundColor);
 
 function changeBackgroundColor(){
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    hexValueText.innerHTML = '#' + randomColor;
     body.style.backgroundColor = '#' + randomColor;
 }
